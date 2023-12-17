42d2d7d261bead0b822cee3d51c51a944cc6a155
AlexxStudio <alexey931931@gmail.com>
2023-12-17 22:46:04 +0300

    Add changes

---
+++ ch1.sh
@@ -0,0 +1,18 @@
+check_1()  {
+	if [[ ! -f "$1" }}; then
+		echo "File not found"
+		exit 1
+	fi
+	file_ext1="${##*.}"
+	if [[ "$file_ext1" != "txt" ]]; then
+		echo "Wrong file format. You must use txt format only"
+		exit 1
+	fi
+	
+	echo "File format is ok"
+	exit 0
+}
+
+for file in "$@"; do
+	check_1 "$file"
+done
